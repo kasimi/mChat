331248bf8b5be586304e3234fa997bbfb94b2188
kasimi <mail@kasimi.net>
2018-05-19 14:08:43 +0200

    Prepare custom CSS & JS for styles by Arty

--- styles/all/template/javascript/mchat.collapse.arty.js
+++ styles/all/template/javascript/mchat.collapse.arty.js
@@ -1,5 +1,7 @@
 /**
  *
+ * mchat.collapse.js - Collapse mChat container for styles by Arty
+ *
  * @package phpBB Extension - mChat
  * @copyright (c) 2017 kasimi - https://kasimi.net
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
@@ -10,10 +12,10 @@ jQuery(function($) {
 
 	"use strict";
 
-	// Most code below copied from elegance/template/forum_fn.js for phpBB 3.2.1
+	// Most code below copied from elegance/template/forum_fn.js for phpBB 3.2.2
 
-	var $this = $('.mchat-wrapper li.header'),
-		forum = $this.parent().next('#mchat-body'),
+	var $this = $('.mchat-wrapper li.header').first(),
+		forum = $this.closest('.topiclist').next(),
 		id = 'mchat',
 		toggled = false,
 		toggle;
@@ -25,22 +27,24 @@ jQuery(function($) {
 		event.preventDefault();
 		$(mChat).trigger('mchat_collapse_toggle_before', [{collapsed: !toggle}]);
 		if (toggled) {
-			forum.stop(true, true).slideDown(200);
+			forum.stop(true, true).slideDown(200, function() {
+				$(mChat).trigger('mchat_collapse_toggle_after', [{collapsed: toggle}]);
+			});
 			toggled = false;
 			toggle.removeClass('toggled');
 			phpbb.deleteCookie('toggled-' + id, styleConfig.cookieConfig);
-			$(mChat).trigger('mchat_collapse_toggle_after', [{collapsed: toggle}]);
 			return;
 		}
-		forum.stop(true, true).slideUp(200);
+		forum.stop(true, true).slideUp(200, function() {
+			$(mChat).trigger('mchat_collapse_toggle_after', [{collapsed: toggle}]);
+		});
 		toggled = true;
 		toggle.addClass('toggled');
 		phpbb.setCookie('toggled-' + id, '1', styleConfig.cookieConfig);
-		$(mChat).trigger('mchat_collapse_toggle_after', [{collapsed: toggle}]);
 	});
 
 	// Check default state
-	if (phpbb.getCookie('toggled-' + id, styleConfig.cookieConfig) == '1') {
+	if (phpbb.getCookie('toggled-' + id, styleConfig.cookieConfig) === '1') {
 		forum.stop(true, true).slideUp(0);
 		toggled = true;
 		toggle.addClass('toggled');
